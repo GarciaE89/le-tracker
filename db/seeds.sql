4c2a32e3e4d8f13faed0fed2ddbb13274a078f37
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-10-28 08:27:47 -0600

    added tables control stmts fro dept and roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES
   ('Accounting'),
   ('Human Capital'),
@@ -7,18 +7,18 @@ VALUES
   ('Product'),
   ('Securities');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-  ( 'VP' 100000 3),
-  ( 'Associate' 70000 4),
-  ( 'Analyst' 48000 1),
-  ('Senior Analyst' 54000 2),
-  ('Partner' 300000 5);
+  ('VP', 100000, 3),
+  ('Associate', 70000, 4),
+  ('Analyst', 48000, 1),
+  ('Senior Analyst', 54000, 2),
+  ('Partner', 300000, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
 VALUES
   ('Yvonne', 'Carter', 3, 2),
-  ( 'Josh', 'Lee', 4, NULL),
+  ('Josh', 'Lee', 4, NULL),
   ('David', 'Stark', 5, 1),
   ('Hunter', 'Watson', 2, NULL),
-  ('Oscar', 'Romero', 1, NULL)
+  ('Oscar', 'Romero', 1, NULL);
--- server.js
+++ server.js
@@ -11,17 +11,64 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
+// error handler
 app.use((req, res) => {
   res.status(404).end();
 });
 
-// Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('connected to Database.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
 
+db.connect(err => {
+    if (err) throw err;
+    
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+  
+      function viewOptions() {
+      // Inquirer asks what user what they would like to view
+          inquirer
+          .prompt([
+              {
+                  type: 'list',
+                  message: 'Welcome to the Employee Tracker, please choose from the following options:',
+                  name: 'mainMenu',
+                  choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+              }
+          ])
+          .then(function(data) {
+  
+  
+              // department 
+              if (data.mainMenu === 'View all departments') {
+                  const sql = `SELECT * FROM department`;
+                  
+                  db.query(sql, (err, rows) => {
+                      if (err) {
+                      console.log('error line 48')
+                      return;
+                      }
+                      console.table(rows)
+                      viewOptions()
+                      });
+                  
+              }
+              // roles
+              else if (data.mainMenu === 'View all roles') {
+                  const sql = `SELECT roles.*, department.name 
+                  AS department_name 
+                  FROM roles 
+                  LEFT JOIN department 
+                  ON roles.department_id = department.id`;
+                  
+                  db.query(sql, (err, rows) => {
+                      if (err) {
+                      console.log('error line 64')
+                      return;
+                      }
+                      console.table(rows)
+                      viewOptions()
+                      });
+  
+              }
+             
+  });
+  
